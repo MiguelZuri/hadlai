2d4042c848ca6b0f461bb00a279c9f935f79fc02
Miguel Zuri <zurijv97@gmail.com>
2019-11-09 12:36:58 -0600

    AD mvc Product

---
+++ database/migrations/2019_11_09_162633_products.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('estilo');
+            $table->string('tipo_area');
+            $table->decimal('precio', 8, 2);
+            $table->boolean('existencia');
+            $table->integer('cantidad');
+            $table->string('categoria');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
